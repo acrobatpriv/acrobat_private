7d1dafbf6e515892655f3d78f4bb1c92bfdaa6b1
klee0kai <klee0kai@gmail.com>
2022-04-26 23:55:59 +0300

    crypt configs

--- crypt.py
+++ crypt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import os
 from time import sleep
 
 import autostart
@@ -8,26 +9,26 @@ import argparse
 
 
 def encrypt_file(file, out_file, passw):
-    shell(f"openssl enc -aes-256-cbc -in {file} -out {out_file} -iter 10000000 -pass pass:{passw}")
+    shell(f"openssl enc -aes-256-cbc -in {file} -out {out_file} -iter 100 -pass pass:{passw}")
     pass
 
 
 def decript_file(file, out_file, passw):
-    shell(f"openssl aes-256-cbc -in {file} -out {out_file} -iter 10000000 -pass pass:{passw}")
+    shell(f"openssl enc -aes-256-cbc -d -in {file} -out {out_file} -iter 100 -pass pass:{passw}")
     pass
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Cryp configs')
-    parser.add_argument('d', action='store_true', help="decrypt")
-    parser.add_argument('e', action='store_true', help="encrypt")
+    parser.add_argument('-d', action='store_true', help="decrypt")
+    parser.add_argument('-e', action='store_true', help="encrypt")
     parser.add_argument('--in_file', help='source file', required=True)
     parser.add_argument('--out_file', help='outfile file', required=True)
     parser.add_argument('--passw', help='passw, default take from ~/.local/data/my_config_key')
     args = parser.parse_args()
 
     if args.passw is None:
-        with open("~/.local/data/my_config_key", "r") as f:
+        with open(os.path.expanduser('~') + "/.local/data/my_config_key", "r") as f:
             args.passw = f.readline()
 
     if args.d:
@@ -37,7 +38,7 @@ if __name__ == '__main__':
         encrypt_file(args.in_file, args.out_file, args.passw)
         exit(0)
     else:
-        print("add d or e")
+        print("add -d or -e")
         parser.print_help()
         exit(0)
 
