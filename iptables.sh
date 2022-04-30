1a4572b78d49ae35a2a07bbc3c99ba6410edf39b
klee0kai <klee0kai@gmail.com>
2022-04-30 13:36:51 +0300

    iptables config

---
+++ iptables.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo iptables-restore < /etc/iptables/rules.v4
--- main_loop.py
+++ main_loop.py
@@ -3,7 +3,6 @@ import sys, os
 from time import sleep
 
 import autostart
-import wifi
 from utils.shell_util import shell
 
 
@@ -16,10 +15,13 @@ def pull_git():
 
 if __name__ == '__main__':
     me_file = os.path.abspath(sys.argv[0])
-    print("pwd: " + os.path.dirname(os.path.realpath(__file__)))
+    pwd = os.path.dirname(os.path.realpath(__file__))
+    print("pwd: " + pwd)
 
     commit = shell("git rev-parse HEAD")
     print(f"acrobat started on commit {commit}")
+
+    shell("sudo " + os.path.join(pwd, "iptables.sh"))
 
     autostart.setup_autostart()
 
@@ -28,7 +30,5 @@ if __name__ == '__main__':
             print(f"acrobat restart after update from remote")
             os.execl("/usr/bin/python3", "/usr/bin/python3", sys.argv[0])
             exit(0)
-
-        wifi.setup_wifi()
 
         sleep(60 * 60 * 24)
